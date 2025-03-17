07c104aaff289870c66fb3f6136d72bcf5eb7de9
Mo.Anas Ali <138597743+anasali2003@users.noreply.github.com>
2025-03-18 04:02:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+
+// app.get('/',(req, res) => {
+//     res.send('Hello');
+// });
+app.get('/app/jokes',(req, res)=>{
+    const jokes = [
+        {
+        id : 1,
+        title: 'Joke 1',
+        content : 'okay'
+     },
+        {
+        id : 2,
+        title: 'Joke 21',
+        content : 'okay'
+     },
+        {
+        id : 3,
+        title: 'Joke 31',
+        content : 'okay'
+     }
+    ];
+    res.send(jokes);
+
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Server at http://localhost:${port}`);
+})
